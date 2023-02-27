f2951e63c18571282db7456674e5b8889c6f4bcb
brt518 <124933379+brt518@users.noreply.github.com>
2023-02-27 18:51:41 -0500

    Added JS Template for daily forecast

--- vanilla-weather-app/src/app.js
+++ vanilla-weather-app/src/app.js
@@ -1,3 +1,29 @@
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  forecastHTML =
+    forecastHTML +
+    `
+      <div class="col-2">
+        <div class="weather-forecast-date">Thu</div>
+        <img
+          src="http://openweathermap.org/img/wn/50d@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> 18° </
+          span>
+          <span class="weather-forecast-temperature-min"> 12° </
+          span>
+        </div>
+      </div>
+  `;
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function formatDate(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
